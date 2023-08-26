196dfe5c6eabb9309c64c1618326f33517fad0e6
skywind3000 <skywind3000@163.com>
2023-08-27 03:45:17 +0800

    update init.lua

--- neovim.lua
+++ neovim.lua
@@ -1,8 +1,21 @@
+--=====================================================================
+--
+-- neovim.lua - 
+--
+-- Created by skywind on 2023/08/27
+-- Last Modified: 2023/08/27 03:19:59
+--
+--=====================================================================
 local scriptpath = debug.getinfo(1, 'S').source:sub(2)
 local scripthome = vim.fn.fnamemodify(scriptpath, ':h')
 
 vim.opt.rtp:prepend(scripthome)
+package.path = package.path .. ';' .. scripthome .. '/lua/?.lua'
 
+
+-----------------------------------------------------------------------
+-- ensure lazy
+-----------------------------------------------------------------------
 function lazy_ensure_install()
 	local lazypath = vim.fn.expand('~/.vim/lazy.nvim')
 	local refresh = false
@@ -22,6 +35,24 @@ function lazy_ensure_install()
 end
 
 local refresh_install = lazy_ensure_install()
+
+
+-----------------------------------------------------------------------
+-- lazy options
+-----------------------------------------------------------------------
+local opts = {
+	performance = {
+		rtp = {
+			reset = false
+		}
+	}
+}
+
+
+-----------------------------------------------------------------------
+-- lazy setup
+-----------------------------------------------------------------------
+require('lazy').setup("plugins", opts)
 
 
 
