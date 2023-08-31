b1d89f886bca7044c824e1c25a5c17f4a6a2218c
skywind3000 <skywind3000@163.com>
2023-08-31 16:29:18 +0800

    update packages.lua

--- lua/plugins/packages.lua
+++ lua/plugins/packages.lua
@@ -35,13 +35,27 @@ return {
 		end
 	},
 
-	{
-		'kshenoy/vim-signature',
-	},
+	{ 'kshenoy/vim-signature', },
+	{ 'mhinz/vim-signify', },
 
 	{
-		'mhinz/vim-signify',
+		'akinsho/toggleterm.nvim', 
+		version = "*",
+		config = function()
+			local toggleterm = require('toggleterm')
+			toggleterm.setup()
+			vim.cmd [[
+			tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+
+			" By applying the mappings this way you can pass a count to your
+			" mapping to open a specific window.
+			" For example: 2<C-t> will open terminal 2
+			nnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+			inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
+			]]
+		end,
 	},
+
 }
 
 
