c2bc3d01d1eebe38d73967fd6e6fcfb1f83a0191
skywind3000 <skywind3000@163.com>
2023-11-09 02:50:52 +0800

    update filters

--- site/text/remove_markdown_images.py
+++ site/text/remove_markdown_images.py
@@ -3,7 +3,7 @@ import re
 import codecs
 
 content = '''
-hello[world](https://world.com)!!
+hello![world](https://world.com)!!
 '''
 
 sys.stdin.reconfigure(encoding = 'utf-8')
@@ -11,10 +11,11 @@ sys.stdout.reconfigure(encoding = 'utf-8')
 
 content = sys.stdin.read()
 
-pattern = r'\[(.*?)\]\(.*?\)'
-new_content = re.sub(pattern, r'\1', content)
+pattern = r'\!\[(.*?)\]\(.*?\)'
+new_content = re.sub(pattern, r'', content)
 
 sys.stdout.write(new_content)
+
 
 
 
